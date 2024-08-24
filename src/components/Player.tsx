390e1c3ec2c000ca154fd70fbe21bbee1b8729b6
Adam Evans <adam3van5@gmail.com>
2024-08-24 20:24:19 +0900

    add button component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Slider from './Slider';
+import Button from './Button';
 
 const Player = React.forwardRef(function Player(
   props: { className?: string },
@@ -46,7 +47,7 @@ const Player = React.forwardRef(function Player(
       </div>
       <div className="bg-slate-50 text-slate-500 dark:bg-slate-600 dark:text-slate-200 rounded-b-xl flex items-center">
         <div className="flex-auto flex items-center justify-evenly">
-          <button type="button" aria-label="Add to favorites">
+          <Button aria-label="Add to favorites">
             <svg width="24" height="24">
               <path
                 d="M7 6.931C7 5.865 7.853 5 8.905 5h6.19C16.147 5 17 5.865 17 6.931V19l-5-4-5 4V6.931Z"
@@ -57,9 +58,8 @@ const Player = React.forwardRef(function Player(
                 strokeLinejoin="round"
               />
             </svg>
-          </button>
-          <button
-            type="button"
+          </Button>
+          <Button
             className="hidden sm:block lg:hidden xl:block"
             aria-label="Previous"
           >
@@ -80,8 +80,8 @@ const Player = React.forwardRef(function Player(
                 strokeLinejoin="round"
               />
             </svg>
-          </button>
-          <button type="button" aria-label="Rewind 10 seconds">
+          </Button>
+          <Button aria-label="Rewind 10 seconds">
             <svg width="24" height="24" fill="none">
               <path
                 d="M6.492 16.95c2.861 2.733 7.5 2.733 10.362 0 2.861-2.734 2.861-7.166 0-9.9-2.862-2.733-7.501-2.733-10.362 0A7.096 7.096 0 0 0 5.5 8.226"
@@ -98,20 +98,19 @@ const Player = React.forwardRef(function Player(
                 strokeLinejoin="round"
               />
             </svg>
-          </button>
+          </Button>
         </div>
-        <button
-          type="button"
-          className="bg-white text-slate-900 dark:bg-slate-100 dark:text-slate-700 flex-none -my-2 mx-auto w-20 h-20 rounded-full ring-1 ring-slate-900/5 shadow-md flex items-center justify-center"
+        <Button
+          className="bg-white text-slate-900 dark:bg-slate-100 dark:text-slate-700 flex-none -my-2 mx-auto w-20 h-20 rounded-full border-2 border-slate-900/5 shadow-md flex items-center justify-center"
           aria-label="Pause"
         >
           <svg width="30" height="32" fill="currentColor">
             <rect x="6" y="4" width="4" height="24" rx="2" />
             <rect x="20" y="4" width="4" height="24" rx="2" />
           </svg>
-        </button>
+        </Button>
         <div className="flex-auto flex items-center justify-evenly">
-          <button type="button" aria-label="Skip 10 seconds">
+          <Button aria-label="Skip 10 seconds">
             <svg width="24" height="24" fill="none">
               <path
                 d="M17.509 16.95c-2.862 2.733-7.501 2.733-10.363 0-2.861-2.734-2.861-7.166 0-9.9 2.862-2.733 7.501-2.733 10.363 0 .38.365.711.759.991 1.176"
@@ -128,9 +127,8 @@ const Player = React.forwardRef(function Player(
                 strokeLinejoin="round"
               />
             </svg>
-          </button>
-          <button
-            type="button"
+          </Button>
+          <Button
             className="hidden sm:block lg:hidden xl:block"
             aria-label="Next"
           >
@@ -151,13 +149,10 @@ const Player = React.forwardRef(function Player(
                 strokeLinejoin="round"
               />
             </svg>
-          </button>
-          <button
-            type="button"
-            className="rounded-lg text-xs leading-6 font-semibold px-2 ring-2 ring-inset ring-slate-500 text-slate-500 dark:text-slate-100 dark:ring-0 dark:bg-slate-500"
-          >
+          </Button>
+          <Button className="rounded-lg text-xs leading-6 font-semibold px-2 border-2 border-slate-500 text-slate-500 dark:text-slate-100 dark:ring-0 dark:bg-slate-500 hover:ring-cyan-500">
             1x
-          </button>
+          </Button>
         </div>
       </div>
     </div>
